066569f1baa46263dce5a4937cd0b5eb6db98fac
K Mamatha <mamatha.k@questdot.io>
2021-07-30 10:03:53 +0530

    second commit

---
+++ js/script.js
@@ -0,0 +1,179 @@
+(function ($) {
+    'use strict';
+
+    var form = $('.contact__form'),
+        message = $('.contact__msg'),
+        form_data;
+
+    // Success function
+    function done_func(response) {
+        message.fadeIn().removeClass('alert-danger').addClass('alert-success');
+        message.text(response);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+        form.find('input:not([type="submit"]), textarea').val('');
+    }
+
+    // fail function
+    function fail_func(data) {
+        message.fadeIn().removeClass('alert-success').addClass('alert-success');
+        message.text(data.responseText);
+        setTimeout(function () {
+            message.fadeOut();
+        }, 2000);
+    }
+    
+    form.submit(function (e) {
+        e.preventDefault();
+        form_data = $(this).serialize();
+        $.ajax({
+            type: 'POST',
+            url: form.attr('action'),
+            data: form_data
+        })
+        .done(done_func)
+        .fail(fail_func);
+    });
+    
+})(jQuery);
+
+
+;(function ($) {
+
+	'use strict';
+
+
+	$('.portfolio-single-slider').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+
+	});
+
+	$('.clients-logo').slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 2000
+	});
+
+	$('.testimonial-wrap').slick({
+		slidesToShow: 2,
+		slidesToScroll: 2,
+		infinite: true,
+		dots: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 6000,
+		responsive: [
+		    {
+		      breakpoint: 1024,
+		      settings: {
+		        slidesToShow: 3,
+		        slidesToScroll: 3,
+		        infinite: true,
+		        dots: true
+		      }
+		    },
+		    {
+		      breakpoint: 900,
+		      settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		      }
+		    },{
+		      breakpoint: 600,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    },
+		    {
+		      breakpoint: 480,
+		      settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1
+		      }
+		    }
+		  
+  		]
+	});
+	$(document).ready(function(){
+
+		$('.items').slick({
+		dots: true,
+		infinite: true,
+		speed: 800,
+		autoplay: true,
+		autoplaySpeed: 2000,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+		{
+		breakpoint: 1024,
+		settings: {
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		infinite: true,
+		dots: true
+		}
+		},
+		{
+		breakpoint: 600,
+		settings: {
+		slidesToShow: 2,
+		slidesToScroll: 2
+		}
+		},
+		{
+		breakpoint: 480,
+		settings: {
+		slidesToShow: 1,
+		slidesToScroll: 1
+		}
+		}
+		
+		]
+		});
+		});
+
+
+   $('.portfolio-gallery').each(function () {
+        $(this).find('.popup-gallery').magnificPopup({
+            type: 'image',
+            gallery: {
+                enabled: true
+            }
+        });
+    });
+
+
+	var map;
+
+	function initialize() {
+		var mapOptions = {
+			zoom: 13,
+			center: new google.maps.LatLng(50.97797382271958, -114.107718560791)
+			// styles: style_array_here
+		};
+		map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	}
+
+	var google_map_canvas = $('#map-canvas');
+
+	if (google_map_canvas.length) {
+		google.maps.event.addDomListener(window, 'load', initialize);
+	}
+
+	// Counter
+
+	$('.counter-stat').counterUp({
+	      delay: 10,
+	      time: 1000
+	  });
+
+	
+
+})(jQuery);
